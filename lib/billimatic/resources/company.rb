a18b0bd69a97b5cd513512be7abcccedfda866b5
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2016-04-20 17:34:10 -0300

    Tests for all Company API endpoints.

--- lib/billimatic/resources/company.rb
+++ lib/billimatic/resources/company.rb
@@ -1,6 +1,7 @@
 module Billimatic
   module Resources
     class Company < Base
+      crud :create, :destroy
 
       def initialize(http)
         @collection_name = 'companies'
@@ -9,8 +10,15 @@ module Billimatic
 
       # GET /api/v1/companies/search
       def search(cnpj)
-        http.get("/companies/search", params: {cnpj: cnpj}) do |response|
+        http.get("#{resource_base_path}/search", params: {cnpj: cnpj}) do |response|
           respond_with_collection(response)
+        end
+      end
+
+      # PATCH /api/v1/companies/:id
+      def update(id, params)
+        http.patch("#{resource_base_path}/#{id}", body: params) do |response|
+          respond_with_entity(response)
         end
       end
 
--- spec/billimatic/resources/company_spec.rb
+++ spec/billimatic/resources/company_spec.rb
@@ -56,4 +56,95 @@ describe Billimatic::Resources::Company do
     end
   end
 
+  describe '#create' do
+    let(:params) do
+      {
+        name: 'Acme Inc',
+        cnpj: '46.181.656/0001-59',
+        company_name: 'Acme Incorporated',
+        address: 'Rua Tal',
+        number: '1337',
+        complement: 'apto 42',
+        district: 'Centro',
+        zipcode: '22290080', # always unformatted
+        city: 'Rio de Janeiro',
+        state: 'RJ',
+        ibge_code: '12345',
+        contacts: 'foo@bar.com, spam@eggs.co.uk',
+        billing_contacts: 'baz@bambam.com.br',
+        comments: 'Algum comentário'
+      }
+    end
+    it 'creates a new company' do
+      VCR.use_cassette('companies/create/success') do
+        company = subject.create(params)
+        expect(company).to be_a(entity_klass)
+        expect(company.id).to_not be_nil
+        expect(company.cnpj).to eq(params[:cnpj])
+      end
+    end
+    it 'raises an error if validation fails' do
+      params[:zipcode] = '22290-080' # will fail validation
+      VCR.use_cassette('companies/create/failure_validation') do
+        expect { subject.create(params) }.to raise_error(
+          Billimatic::RequestError) do |error|
+            expect(error.code).to eq(422)
+            expect(error.body['errors']['cnpj']).to eq(['já está em uso'])
+            expect(error.body['errors']['zipcode']).
+              to eq(['Por favor digite um CEP válido.'])
+          end
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:id) { 567 } # ID of the company created on the #create test above
+    let(:params) do
+      {address: 'Outra Rua', number: '999'}
+    end
+    it 'updates an existing company' do
+      VCR.use_cassette('companies/update/success') do
+        company = subject.update(id, params)
+        expect(company).to be_a(entity_klass)
+        expect(company.id).to eq(id)
+        expect(company.address).to eq(params[:address])
+        expect(company.number).to eq(params[:number])
+        expect(company.name).to eq('Acme Inc') # not changed
+      end
+    end
+    it 'raises an error if validation fails' do
+      params[:zipcode] = '22290-080' # will fail validation
+      VCR.use_cassette('companies/update/failure_validation') do
+        expect { subject.update(id, params) }.to raise_error(
+          Billimatic::RequestError) do |error|
+            expect(error.code).to eq(422)
+            expect(error.body['errors']['zipcode']).
+              to eq(['Por favor digite um CEP válido.'])
+          end
+      end
+    end
+    it 'raises an error if the company does not exist' do
+      VCR.use_cassette('companies/update/failure_not_found') do
+        expect { subject.update(id + 1, params) }.to raise_error(
+          Billimatic::RequestError) { |error| expect(error.code).to eq(404) }
+      end
+    end
+  end
+
+  describe '#destroy' do
+    let(:id) { 567 } # ID of the company created on the #create test above
+    it 'deletes an existing company and returns true' do
+      VCR.use_cassette('companies/destroy/success') do
+        result = subject.destroy(id)
+        expect(result).to be_truthy
+      end
+    end
+    it 'raises an error if the company does not exist' do
+      VCR.use_cassette('companies/destroy/failure_not_found') do
+        expect { subject.destroy(id + 1) }.to raise_error(
+          Billimatic::RequestError) { |error| expect(error.code).to eq(404) }
+      end
+    end
+  end
+
 end
