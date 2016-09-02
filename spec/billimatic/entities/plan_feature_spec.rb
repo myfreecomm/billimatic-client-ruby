901de41173a2556344db0af76e079db1e6315840
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-09-02 14:29:20 -0300

    Adds _destroy attribute to PlanFeature

--- spec/billimatic/entities/plan_feature_spec.rb
+++ spec/billimatic/entities/plan_feature_spec.rb
@@ -6,11 +6,12 @@ describe Billimatic::Entities::PlanFeature do
       id: 1,
       description: 'Até 5 usuários',
       value: 'Foo',
-      tag: 'tag'
+      tag: 'tag',
+      _destroy: false
     }
   end
 
   subject { described_class.new(attributes) }
 
-  it_behaves_like 'entity_attributes', [:id, :description, :value, :tag]
+  it_behaves_like 'entity_attributes', [:id, :description, :value, :tag, :_destroy]
 end
