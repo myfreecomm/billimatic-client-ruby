59896d9a1abdc0df5d545143c875f158e814c857
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-09-19 11:10:31 -0300

    Introduces Contract search endpoint

--- lib/billimatic/resources/contract.rb
+++ lib/billimatic/resources/contract.rb
@@ -3,8 +3,13 @@ module Billimatic
     class Contract < Base
       crud :all
 
-      # TODO...
-      # search
+      def search(name)
+        http.get(
+          "#{resource_base_path}/search", params: { name: name }
+        ) do |response|
+          respond_with_collection response
+        end
+      end
     end
   end
 end
--- spec/billimatic/resources/contract_spec.rb
+++ spec/billimatic/resources/contract_spec.rb
@@ -9,4 +9,65 @@ describe Billimatic::Resources::Contract do
   it 'has a instance of Billimatic::Http' do
     expect(subject.http).to eq(http)
   end
+
+  describe '#search' do
+    context 'sending the full contract name as parameter' do
+      it 'returns a collection with contracts matching the name' do
+        VCR.use_cassette('/contracts/search/success/full_name_matching') do
+          contracts = subject.search('Prestação de Serviço Um')
+
+          expect(contracts).not_to be_empty
+          contracts.each do |contract|
+            expect(contract).to be_a entity_klass
+            expect(contract.name).to eql 'Prestação de Serviço Um'
+          end
+        end
+      end
+    end
+
+    context 'sending part of a contract name as parameter' do
+      it 'returns a collection with contracts matching the search' do
+        VCR.use_cassette('/contracts/search/success/partial_name_matching') do
+          contracts = subject.search('Assinatura')
+
+          expect(contracts).not_to be_empty
+          contracts.each do |contract|
+            expect(contract).to be_a entity_klass
+            expect(contract.name).to match /Assinatura/
+          end
+        end
+      end
+    end
+
+    context 'sending a name with no matches' do
+      it 'returns an empty array as result' do
+        VCR.use_cassette('/contracts/search/success/no_matches_result') do
+          contracts = subject.search('contrato')
+          expect(contracts).to be_empty
+        end
+      end
+    end
+
+    context 'providing no name or empty string for search' do
+      it 'raises Billimatic::RequestError on empty string search' do
+        VCR.use_cassette('/contracts/search/failure/empty_string_name') do
+          expect {
+            subject.search('')
+          }.to raise_error(Billimatic::RequestError) do |error|
+            expect(error.code).to eql 400
+          end
+        end
+      end
+
+      it 'raises Billimatic::RequestError on request without name parameter' do
+        VCR.use_cassette('/contracts/search/failure/null_parameter') do
+          expect {
+            subject.search(nil)
+          }.to raise_error(Billimatic::RequestError) do |error|
+            expect(error.code).to eql 400
+          end
+        end
+      end
+    end
+  end
 end
