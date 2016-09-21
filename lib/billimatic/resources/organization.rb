9b7b9c7fc260f49d4bf1816929603b5f8faf73c8
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-09-21 13:01:47 -0300

    Introduces Organization create endpoint

--- lib/billimatic/resources/organization.rb
+++ lib/billimatic/resources/organization.rb
@@ -1,6 +1,8 @@
 module Billimatic
   module Resources
     class Organization < Base
+      crud :create
+
       def search(cnpj:)
         http.get("#{resource_base_path}/search", params: {cnpj: cnpj}) do |response|
           respond_with_entity(response)
--- spec/billimatic/resources/organization_spec.rb
+++ spec/billimatic/resources/organization_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Billimatic::Resources::Organization do
   let(:entity_klass) { Billimatic::Entities::Organization }
-  let(:http) { Billimatic::Http.new('d0cb3c0eae88857de3266c7b6dd7298d') }
+  let(:http) { Billimatic::Http.new('6123247039bf2a14e3f1de59626e7b2d') }
 
   subject { described_class.new(http) }
 
@@ -67,6 +67,62 @@ describe Billimatic::Resources::Organization do
           subject.search(cnpj: '05.784.655/0001-11')
         }.to raise_error(Billimatic::RequestError) do |error|
           expect(error.code).to eql 404
+        end
+      end
+    end
+  end
+
+  describe '#create' do
+    it 'successfully creates an organization with formatted cnpj' do
+      VCR.use_cassette('organizations/create/success/formatted_cnpj') do
+        organization = subject.create(
+          name: "Nova Organização", cnpj: "87.667.729/0001-02"
+        )
+
+        expect(organization).to be_a entity_klass
+        expect(organization.name).to eql 'Nova Organização'
+        expect(organization.cnpj).to eql "87.667.729/0001-02"
+      end
+    end
+
+    it 'successfully creates an organization with unformatted cnpj' do
+      VCR.use_cassette('organizations/create/success/unformatted_cnpj') do
+        organization = subject.create(
+          name: "Nova Organização com CNPJ sem formato", cnpj: "25029551000109"
+        )
+
+        expect(organization).to be_a entity_klass
+        expect(organization.name).to eql 'Nova Organização com CNPJ sem formato'
+        expect(organization.cnpj).to eql "25.029.551/0001-09"
+      end
+    end
+
+    it 'raises Billimatic::RequestError if request is invalid' do
+      VCR.use_cassette('organizations/create/failure/invalid_parameters') do
+        expect {
+          subject.create(cnpj: "87.667.729/0001-02")
+        }.to raise_error(Billimatic::RequestError) do |error|
+          expect(error.code).to eql 422
+        end
+      end
+    end
+
+    it 'raises Billimatic::RequestError when trying to create a duplicate organization' do
+      VCR.use_cassette('organizations/create/failure/duplicate_organization') do
+        expect {
+          subject.create(name: 'Organização', cnpj: "53.724.867/0001-56")
+        }.to raise_error(Billimatic::RequestError) do |error|
+          expect(error.code).to eql 422
+        end
+      end
+    end
+
+    it 'raises Billimatic::RequestError when cnpj is invalid' do
+      VCR.use_cassette('organizations/create/failure/invalid_cnpj') do
+        expect {
+          subject.create(name: 'Nova Organização', cnpj: "foo")
+        }.to raise_error(Billimatic::RequestError) do |error|
+          expect(error.code).to eql 422
         end
       end
     end
