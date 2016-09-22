47afd6797b6731e94e13864f15c324383ef949e8
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-09-22 09:20:37 -0300

    Introduces Receivable entity

---
+++ spec/billimatic/entities/receivable_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Billimatic::Entities::Receivable do
+  let(:attributes) do
+    {
+      id: 1,
+      invoice_id: 2,
+      due_date: '2016-09-22',
+      value: 100.0,
+      gross_value: 100.0,
+      payment_value: 90.0,
+      received_value: 90.0,
+      received_at: '2016-09-21',
+      state: 'received',
+      cobrato_charge_id: 1
+    }
+  end
+
+  subject { described_class.new(attributes) }
+
+  it_behaves_like 'entity_attributes', [
+                    :id, :invoice_id, :due_date, :value, :gross_value,
+                    :payment_value, :received_value, :received_at,
+                    :created_at, :state, :payment_gateway_status,
+                    :cobrato_charge_id, :cobrato_errors, :processing_on_cobrato,
+                    :waiting_cobrato_registration, :finance_receivable_id,
+                    :finance_entity_id, :finance_errors, :processing_on_myfinance
+                  ]
+end
