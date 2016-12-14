0b044123ce3c8992eba84825d8c0089640e41b99
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-12-14 11:09:58 -0200

    Allows optional cancellation params on Subscription#cancel

--- spec/billimatic/resources/subscription_spec.rb
+++ spec/billimatic/resources/subscription_spec.rb
@@ -394,11 +394,61 @@ describe Billimatic::Resources::Subscription do
   end
 
   describe '#cancel' do
-    it "successfully sets a subscription to 'cancelled'" do
-      VCR.use_cassette('subscriptions/cancel/success') do
-        subscription = subject.cancel(token: 'f7e385a902a9f626addacdcccc90f10e')
+    before do
+      Billimatic.configuration.host = "http://localhost:3000"
+      Typhoeus::Expectation.clear
+      @http = Billimatic::Http.new('d0cb3c0eae88857de3266c7b6dd7298d')
+    end
+
+    subject { described_class.new(@http) }
+
+    it "successfully sets a subscription to 'cancelled' and cancel_date as today" do
+      VCR.use_cassette('subscriptions/cancel/success/without_params') do
+        subscription = subject.cancel(token: 'd3e7349eede8cc8b030f76319cfa6f8d')
 
         expect(subscription.status).to eql 'cancelled'
+        expect(subscription.cancel_date).to eql Date.parse('2016-12-14')
+        expect(subscription.cancel_reason).to be_nil
+      end
+    end
+
+    it 'successfully cancels subscription and sets cancel_date' do
+      VCR.use_cassette('subscriptions/cancel/success/with_cancel_date') do
+        subscription = subject.cancel(
+          token: 'e5631118c9be24fef01b965ff3649483',
+          cancel_date: Date.parse("15/12/2016")
+        )
+
+        expect(subscription.status).to eql 'cancelled'
+        expect(subscription.cancel_date).to eql Date.parse('2016-12-15')
+        expect(subscription.cancel_reason).to be_nil
+      end
+    end
+
+    it 'successfully cancels subscription and sets cancel_reason' do
+      VCR.use_cassette('subscriptions/cancel/success/with_cancel_reason') do
+        subscription = subject.cancel(
+          token: '3f9a984162f24e9f1dbda58fdbc2fd8f',
+          cancel_reason: "Cancelamento via API de e-commerce"
+        )
+
+        expect(subscription.status).to eql 'cancelled'
+        expect(subscription.cancel_date).to eql Date.parse('2016-12-14')
+        expect(subscription.cancel_reason).to eql "Cancelamento via API de e-commerce"
+      end
+    end
+
+    it 'successfully cancels subscription and sets both cancellation params' do
+      VCR.use_cassette('subscriptions/cancel/success/with_cancellation_params') do
+        subscription = subject.cancel(
+          token: '96f1bb875ebc1b972bcbd3c60f45efe9',
+          cancel_date: Date.parse("15/12/2016"),
+          cancel_reason: "Cancelamento via API de e-commerce"
+        )
+
+        expect(subscription.status).to eql 'cancelled'
+        expect(subscription.cancel_date).to eql Date.parse('2016-12-15')
+        expect(subscription.cancel_reason).to eql "Cancelamento via API de e-commerce"
       end
     end
 
