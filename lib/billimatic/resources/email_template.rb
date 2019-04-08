d84ba1aaad91d1aaff3a0f342ea8e616eb7a005f
Raul Fernando <raulfernando08@gmail.com>
2019-04-08 10:50:21 -0300

    Adjusts EmailTemplate to use inherited method #list_by_organization

--- lib/billimatic/resources/email_template.rb
+++ lib/billimatic/resources/email_template.rb
@@ -1,11 +1,7 @@
 module Billimatic
   module Resources
     class EmailTemplate < Base
-      def list(organization_id:)
-        http.get(
-          "/organizations/#{organization_id}#{resource_base_path}"
-        ) { |response| respond_with_collection(response) }
-      end
+      crud :list_by_organization
     end
   end
 end
